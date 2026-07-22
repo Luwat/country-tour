673cf49056fc41d4080908095a177f928a523ea3
Oluwatobi <oluwatobioreniyi@gmail.com>
2026-07-22 22:52:29 +0100

    Refactor Countries and Root components to align with new API structure
    - Updated Countries component to use new data structure for country objects.
    - Changed filtering logic to accommodate updated field names.
    - Adjusted rendering of country details to reflect new API response format.
    - Modified Root component to fetch data from a proxy endpoint and handle response correctly.

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Outlet, json } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
 export default function RootLayout() {
@@ -12,18 +12,19 @@ export default function RootLayout() {
 }
 
 export async function loadCountry() {
-  const response = await fetch(
-    "https://restcountries.com/v5?response_fields=names,flag,population,region,capitals",
-    {
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_REST_COUNTRIES_API_KEY}`,
-      },
+  const response = await fetch("/.netlify/functions/api-proxy", {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_REST_COUNTRIES_API_KEY}`,
     },
-  );
+  });
 
   if (!response.ok) {
-    throw json({ message: "Could not fetch data" }, { status: 500 });
+    throw new Response(JSON.stringify({ message: "Could not fetch data" }), {
+      status: 500,
+    });
   }
 
-  return response;
+  const { data } = await response.json();
+
+  return data;
 }
